67fa30fc64d7b8da0c60a428666bf7950aa847dc
Sabuj Hasan Sarker <sabujhasansarker@gmail.com>
2020-10-02 11:33:48 +0600

    categories

---
+++ src/components/pages/home/Categories.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+import categories from "../../layouts/Nav.json";
+import { Link } from "react-router-dom";
+
+const Category = ({ container }) => {
+  return (
+    <div className={`categories ${container && "container"}`}>
+      <h2 className="text-left">Top Categories</h2>
+      <div className="categories-container">
+        {categories.map((category, index) => (
+          <Link
+            key={index}
+            to={category.name.split(" ").join("-").toLowerCase()}
+            className="category"
+          >
+            <img src={category.img} alt={category.name} />
+            <h3>{category.name}</h3>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Category;
--- src/components/pages/home/Home.js
+++ src/components/pages/home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import Banner from "./Banner";
 import Courses from "./Courses";
 import GetStart from "./GetStart";
+import Categories from "./Categories";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <Banner container={true} />
       <Courses container={true} />
       <GetStart container={false} />
+      <Categories container={true} />
     </div>
   );
 };
